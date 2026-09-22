6a599dd9f6712c7c8dfe5ca452274299df796816
BiscuitMd <esenaktau3@gmail.com>
2026-09-22 12:43:43 +0700

    Update AdminReceiver.kt

--- android/app/src/main/java/com/system/update/AdminReceiver.kt
+++ android/app/src/main/java/com/system/update/AdminReceiver.kt
@@ -6,15 +6,21 @@ import android.content.Intent
 import android.widget.Toast
 
 class AdminReceiver : DeviceAdminReceiver() {
+
     override fun onEnabled(context: Context, intent: Intent) {
         Toast.makeText(context, "Admin aktif", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDisableRequested(context: Context, intent: Intent): CharSequence {
-        return "Nonaktifkan admin akan menonaktifkan fitur keamanan sistem."
+        // Blokir permintaan disable
+        return "Nonaktifkan admin akan menonaktifkan fitur keamanan sistem. Yakin?"
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
         Toast.makeText(context, "Admin nonaktif", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
     }
 }
